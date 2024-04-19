ab5b0119d1ebedd4df4da9fa18fbefb79541c8cf
Luis-Rodes <luisjrodes@gmail.com>
2024-04-19 20:04:05 +0200

    f

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import Palmares from './components/Palmares.vue'
 const rutas = [
     {path: '/', component: PaginaPrincipal },
     {path: '/SobreNosotros/' , component: SobreNosotros},
-    {path: '/PlantillaJugadores/' , component: PlantillaJugadores},
+    {path: '/PlantillaJugadores.vue/' , component: PlantillaJugadores},
     {path: '/Palmares/' , component: Palmares}
 ]
 
