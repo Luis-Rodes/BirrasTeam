121af93b6881e18d010700a95e97bcc127c24908
Luis-Rodes <luisjrodes@gmail.com>
2024-05-07 19:20:21 +0200

    ruta

--- src/components/PlantillaDeJugadores.vue
+++ src/components/PlantillaDeJugadores.vue
@@ -37,7 +37,7 @@
             </div>
             <div class="col">
               <div class="card h-100">
-                <img src="../assets/crash.svg" class="card-img-top" alt="...">
+                <img src="../assets/Crash.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
